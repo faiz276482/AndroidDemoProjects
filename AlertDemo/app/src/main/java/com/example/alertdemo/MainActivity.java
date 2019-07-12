7274a3bcf8c87721bf5bafbcd36bcbd265a7464d
Faiz Alam <faiz276482@gmail.com>
2019-07-12 19:55:36 +0530

    Alert Demo app

---
+++ AlertDemo/app/src/main/java/com/example/alertdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.alertdemo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure!?")
+                .setMessage("Do you definitely want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Its Done!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No",null)
+                .show();
+    }
+}
