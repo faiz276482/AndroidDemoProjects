c2f0f18e923d60653ee4eff0f72f4cc28cc7a04c
Faiz Alam <faiz276482@gmail.com>
2019-07-13 21:45:47 +0530

    SQLLitedatabse demo updted

--- SQLLiteDatabaseDemo/app/src/main/java/com/example/sqllitedatabasedemo/MainActivity.java
+++ SQLLiteDatabaseDemo/app/src/main/java/com/example/sqllitedatabasedemo/MainActivity.java
@@ -15,19 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try{
-            SQLiteDatabase sqLiteDatabase=this.openOrCreateDatabase("Events",MODE_PRIVATE,null);
-            sqLiteDatabase.execSQL("Create table If not exists  events (name varchar, year int(4))");
-            sqLiteDatabase.execSQL("Insert into events (name,year) values('Milenium',2000)");
-            sqLiteDatabase.execSQL("Insert into events (name,year) values('I started college',2017)");
+            SQLiteDatabase sqLiteDatabase=this.openOrCreateDatabase("Users",MODE_PRIVATE,null);
+            sqLiteDatabase.execSQL("Create table If not exists  newUsers (name varchar, year int(4),id Integer Primary Key)");
+            sqLiteDatabase.execSQL("Insert into newUsers (name,age) values('Zaid',21)");
+            sqLiteDatabase.execSQL("Insert into newUsers (name,age) values('Damini',22)");
+            sqLiteDatabase.execSQL("Insert into newUsers (name,age) values('Faiz',22)");
+            sqLiteDatabase.execSQL("Insert into newUsers (name,age) values('Anjali',21)");
 
-            Cursor c=sqLiteDatabase.rawQuery("Select * from events",null);
-            int eventIndex=c.getColumnIndex("name");
-            int yearIndex=c.getColumnIndex("year");
+            Cursor c=sqLiteDatabase.rawQuery("Select * from newUsers where name like '%a%'",null);
+            int nameIndex=c.getColumnIndex("name");
+            int ageIndex=c.getColumnIndex("age");
+            int idIndex=c.getColumnIndex("id");
             c.moveToFirst();
             while (c!=null)
             {
-                Log.i("event",c.getString(eventIndex));
-                Log.i("year",c.getString(yearIndex));
+                Log.i("User",c.getString(nameIndex));
+                Log.i("age",c.getString(ageIndex));
+                Log.i("ID",c.getString(idIndex));
                 c.moveToNext();
             }
             c.close();
